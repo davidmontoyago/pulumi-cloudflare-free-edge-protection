72a353f4851866eec7e3c49bcb139a84a9929b82
David Montoya <davidmontoyago@gmail.com>
2025-08-21 13:37:15 -0500

    fix format for waf expressions

--- pkg/cloudflare/waf.go
+++ pkg/cloudflare/waf.go
@@ -2,6 +2,7 @@ package cloudflare
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/pulumi/pulumi-cloudflare/sdk/v6/go/cloudflare"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
@@ -86,45 +87,24 @@ func (e *EdgeProtection) createWAFCustomRules(ctx *pulumi.Context, zone *cloudfl
 			// Rule 1: Block common CMS attack patterns (WordPress, etc.)
 			// TODO add more paths and extensions
 			&cloudflare.RulesetRuleArgs{
-				Action: pulumi.String("block"),
-				Expression: pulumi.String(`
-									(http.request.uri.path contains "/wp-admin") or
-									(http.request.uri.path contains "/wp-login.php") or
-									(http.request.uri.path contains "/xmlrpc.php") or
-									(http.request.uri.path contains "/.env") or
-									(http.request.uri.path contains "/phpmyadmin") or
-									(http.request.uri.path contains "/config.php") or
-									(http.request.uri.path contains "/wp-config.php")
-							`),
+				Action:      pulumi.String("block"),
+				Expression:  pulumi.String(`(http.request.uri.path matches "\/[wp\-admin|wp\-login.php|xmlrpc.php|\.env|phpmyadmin|config.php|wp\-config.php]*$")`),
 				Description: pulumi.String("Block common CMS and config file access attempts"),
 				Enabled:     pulumi.Bool(true),
 			},
 
 			// Rule 2: Block malicious user agents and tools
 			&cloudflare.RulesetRuleArgs{
-				Action: pulumi.String("block"),
-				Expression: pulumi.String(`
-									(http.user_agent contains "sqlmap") or
-									(http.user_agent contains "nmap") or
-									(http.user_agent contains "nikto") or
-									(http.user_agent contains "masscan") or
-									(http.user_agent contains "dirbuster") or
-									(http.user_agent eq "") or
-									(len(http.user_agent) < 10)
-							`),
+				Action:      pulumi.String("block"),
+				Expression:  pulumi.String(`(http.user_agent matches "\/[sqlmap|nmap|nikto|masscan|dirbuster]*$") or (http.user_agent eq "") or (len(http.user_agent) < 10)`),
 				Description: pulumi.String("Block malicious scanners and empty user agents"),
 				Enabled:     pulumi.Bool(true),
 			},
 
 			// Rule 3: Block dangerous HTTP methods and protocols
 			&cloudflare.RulesetRuleArgs{
-				Action: pulumi.String("block"),
-				Expression: pulumi.String(`
-									(http.request.method eq "TRACE") or
-									(http.request.method eq "TRACK") or
-									(http.request.method eq "DEBUG") or
-									(http.request.method eq "CONNECT")
-							`),
+				Action:      pulumi.String("block"),
+				Expression:  pulumi.String(`(http.request.method eq "TRACE") or (http.request.method eq "TRACK") or (http.request.method eq "DEBUG") or (http.request.method eq "CONNECT")`),
 				Description: pulumi.String("Block dangerous HTTP methods"),
 				Enabled:     pulumi.Bool(true),
 			},
@@ -132,15 +112,15 @@ func (e *EdgeProtection) createWAFCustomRules(ctx *pulumi.Context, zone *cloudfl
 			// Rule 4: Challenge suspicious bot behavior
 			&cloudflare.RulesetRuleArgs{
 				Action: pulumi.String("managed_challenge"),
-				Expression: pulumi.String(`
-									(http.request.uri.query contains "union select") or
-									(http.request.uri.query contains "drop table") or
-									(http.request.uri.query contains "insert into") or
-									(http.request.uri.query contains "<script") or
-									(http.request.uri.query contains "javascript:") or
-									(http.request.uri.path contains "..") or
-									(http.request.uri.path contains "%2e%2e")
-							`),
+				Expression: pulumi.Sprintf("(%s)", strings.Join([]string{
+					`(http.request.uri.query contains "union select")`,
+					`(http.request.uri.query contains "drop table")`,
+					`(http.request.uri.query contains "insert into")`,
+					`(http.request.uri.query contains "<script")`,
+					`(http.request.uri.query contains "javascript:")`,
+					`(http.request.uri.path contains "..")`,
+					`(http.request.uri.path contains "%2e%2e")`,
+				}, " or ")),
 				Description: pulumi.String("Challenge requests with SQL injection or XSS patterns"),
 				Enabled:     pulumi.Bool(true),
 			},
@@ -148,15 +128,15 @@ func (e *EdgeProtection) createWAFCustomRules(ctx *pulumi.Context, zone *cloudfl
 			// Rule 5: Block requests to sensitive file types and admin paths
 			&cloudflare.RulesetRuleArgs{
 				Action: pulumi.String("block"),
-				Expression: pulumi.String(`
-									(http.request.uri.path matches ".*\\.(bak|backup|old|orig|tmp|temp|log)$") or
-									(http.request.uri.path contains "/admin") or
-									(http.request.uri.path contains "/administrator") or
-									(http.request.uri.path contains "/.git") or
-									(http.request.uri.path contains "/.svn") or
-									(http.request.uri.path contains "/server-status") or
-									(http.request.uri.path contains "/server-info")
-							`),
+				Expression: pulumi.Sprintf("(%s)", strings.Join([]string{
+					`http.request.uri.path matches ".*\\.(bak|backup|old|orig|tmp|temp|log)$")`,
+					`(http.request.uri.path contains "/admin")`,
+					`(http.request.uri.path contains "/administrator")`,
+					`(http.request.uri.path contains "/.git")`,
+					`(http.request.uri.path contains "/.svn")`,
+					`(http.request.uri.path contains "/server-status")`,
+					`(http.request.uri.path contains "/server-info"`,
+				}, " or ")),
 				Description: pulumi.String("Block access to sensitive files and admin paths"),
 				Enabled:     pulumi.Bool(true),
 			},
